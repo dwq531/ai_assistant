907ed681819a2a2b55633b8855733f36d142b6b0
ruoqizheng <57671392+ruoqizheng@users.noreply.github.com>
2023-09-14 14:57:22 +0800

    Update image_generate.py

--- image_generate.py
+++ image_generate.py
@@ -1,11 +1,17 @@
 import os
-
+import requests
+import json
 
 def image_generate(content: str):
-    """
-    TODO
-    """
-    pass
+    url = "https://api.openai.com/v1/images/generations"
+    headers = "Content-Type: application/json"
+    params = {
+        "prompt":content,
+        "size":"256x256"
+    }
+    response = requests.get(url, headers=headers, params=params)
+    dict = json.loads(response.text)
+    return dict['data'][0]['url']
 
 if __name__ == "__main__":
     image_generate('A cute baby sea otter')
